e179952c300d1f6dbc08e7703227c53169303d84
nikhil reddy <jnikhilreddy219@gmail.com>
2025-02-10 21:20:52 +0530

    inital commit

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }),
+  React.createElement("h1", {}, "I'm an h1 tag"),
+]);
+console.log("check for the parent", parent);
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from the React"
+);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
